4df82b387e5dd0faf2061d124ee01147a4e8a4b2
stevegotham <g23design@gmail.com>
2016-04-14 17:37:25 +0000

    finished part 2

--- part-2/myCtrl.js
+++ part-2/myCtrl.js
@@ -1,4 +1,6 @@
 app.controller("myCtrl", function($scope) {
+    
+    // ====== change paragraph color ======
     $scope.paragraphs = document.getElementsByTagName('p');
     for (var i=0; i<$scope.paragraphs.length; i++) {
         $scope.paragraphs[i].addEventListener('mouseover', function() {
@@ -8,9 +10,26 @@ app.controller("myCtrl", function($scope) {
             event.target.style="color:black;"
         });
     };
+    
+    // ===== add exclamation point ======
     $scope.shout = function() {
         h1.innerHTML = h1.innerHTML + '!'
-    };
+    }
     
+    // ===== confirm link clicks ======
+    $scope.doubleCheck = function() {
+        var result = confirm("Do you want to go to the link?");
+            if(!result) {
+                event.preventDefault();
+                console.log(this)
+                document.getElementById('body').removeChild(this);
+            }
+        }
+    $scope.links = document.getElementsByTagName('a');
+    for (var i=0; i<$scope.links.length; i++) {
+        $scope.links[i].addEventListener('click', $scope.doubleCheck);
+    }
+
 })  
+
 
